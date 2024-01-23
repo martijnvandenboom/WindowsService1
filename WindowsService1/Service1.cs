b77c748b8256930d336fcc69c2044341443e5d35
ing. M.A.C.M. (Martijn) van den Boom <martijn.vandenboom@icloud.com>
2024-01-23 18:02:23 +0100

    Initial commit

---
+++ WindowsService1/Service1.cs
@@ -0,0 +1,58 @@
+﻿using System.ServiceProcess;
+using System.Diagnostics;
+
+namespace WindowsService1
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            ServiceName = "MyPowerShellService";
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            // Start the PowerShell script in the background
+            StartPowerShellScript();
+        }
+
+        protected override void OnStop()
+        {
+            // Stop the PowerShell script if needed
+        }
+
+        private void StartPowerShellScript()
+        {
+            ProcessStartInfo psi = new ProcessStartInfo
+            {
+                FileName = "powershell.exe",
+                RedirectStandardInput = true,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+
+            Process process = new Process { StartInfo = psi };
+            process.Start();
+
+            // Replace "C:\Path\To\Your\Script\MyScript.ps1" with the actual path to your PowerShell script
+            string scriptPath = "C:\\Windows\\Temp\\MyServiceScript.ps1";
+            string command = $"& '{scriptPath}'";
+
+            process.StandardInput.WriteLine(command);
+            process.StandardInput.Close();
+
+            // Optionally, you can read the output and error streams if needed
+            string output = process.StandardOutput.ReadToEnd();
+            string error = process.StandardError.ReadToEnd();
+
+            process.WaitForExit();
+            process.Close();
+        }
+    }
+}
+
+
+
+
